f937b2bab49f95f234c8508d71cfdf766257a0ac
Adam Williams <Adamleewilliams1989@gmail.com>
2025-05-09 12:25:28 -0400

    add api for healthcheck

---
+++ api/router.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"n3w-sh0p/api/controller"
+
+	"github.com/gin-gonic/gin"
+	// "github.com/roylisto/gin-golang-react/api/controller"
+)
+
+type Router interface {
+	SetupRouter(router *gin.Engine)
+}
+
+type router struct{}
+
+func NewRouter() Router {
+	return &router{}
+}
+
+// SetupRouter configuration router information
+func (r *router) SetupRouter(router *gin.Engine) {
+	var (
+		healthController controller.HealthController = controller.NewHealthController()
+	)
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		// healthcheck
+		v1.GET("/healthcheck", healthController.Check)
+	}
+}
